566a6c9dbba2657a0130f3116f9959c51563b32e
Evgen Orlovsky <orloffski@gmail.com>
2016-11-15 19:40:05 +0300

    BUGFIX prefixes checked

--- app/src/main/java/by/madcat/development/f1newsreader/Utils/StringUtils.java
+++ app/src/main/java/by/madcat/development/f1newsreader/Utils/StringUtils.java
@@ -3,13 +3,13 @@ package by.madcat.development.f1newsreader.Utils;
 import by.madcat.development.f1newsreader.data.DatabaseDescription;
 
 public final class StringUtils {
-    private static final String NEWS_PREFIX = "/news/";
-    private static final String MEMUAR_PREFIX = "/memuar/";
-    private static final String INTERVIEW_PREFIX = "/interview/";
-    private static final String TECH_PREFIX = "/tech/";
-    private static final String HISTORY_PREFIX = "/Championship/";
-    private static final String COLUMNS_PREFIX = "/columns/";
-    private static final String AUTOSPORT_PREFIX = "/autosport-";
+    private static final String NEWS_PREFIX = "f1news.ru/news/";
+    private static final String MEMUAR_PREFIX = "f1news.ru/memuar/";
+    private static final String INTERVIEW_PREFIX = "f1news.ru/interview/";
+    private static final String TECH_PREFIX = "f1news.ru/tech/";
+    private static final String HISTORY_PREFIX = "f1news.ru/Championship/";
+    private static final String COLUMNS_PREFIX = "f1news.ru/columns/";
+    private static final String AUTOSPORT_PREFIX = "f1news.ru/autosport-";
 
     public static String getImageNameFromURL(String url){
         return url.split("/")[url.split("/").length - 1];
