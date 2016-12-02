5e7609e0ed2f4cb52fe6986ce46bfd0567cad902
Evgen Orlovsky <orloffski@gmail.com>
2016-12-02 22:23:01 +0300

    Rev 1.070-3 Update adapters

--- app/src/main/java/by/madcat/development/f1newsreader/adapters/NewsListAbstractAdapter.java
+++ app/src/main/java/by/madcat/development/f1newsreader/adapters/NewsListAbstractAdapter.java
@@ -1,12 +1,42 @@
 package by.madcat.development.f1newsreader.adapters;
 
+import android.content.Context;
 import android.database.Cursor;
 import android.support.v7.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.io.File;
+
+import by.madcat.development.f1newsreader.data.DatabaseDescription;
+import by.madcat.development.f1newsreader.dataInet.LoadNewsTask;
 
 public abstract class NewsListAbstractAdapter extends RecyclerView.Adapter<ViewHolder>{
     public interface ClickListener{
         void onClick(int positionID);
     }
 
-    public abstract void swapCursor(Cursor cursor);
+    protected Cursor cursor;
+    protected Context context;
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        cursor.moveToPosition(position);
+        holder.setRowID(position);
+        holder.title.setText(cursor.getString(cursor.getColumnIndex(DatabaseDescription.News.COLUMN_TITLE)));
+
+        String pathToImage = context.getFilesDir() +
+                "/" + LoadNewsTask.IMAGE_PATH + "/" + cursor.getString(cursor.getColumnIndex(DatabaseDescription.News.COLUMN_IMAGE));
+        Glide.with(context).load(new File(pathToImage)).into(holder.thumbnail);
+    }
+
+    @Override
+    public int getItemCount() {
+        return (cursor != null) ? cursor.getCount() : 0;
+    }
+
+    public void swapCursor(Cursor cursor){
+        this.cursor = cursor;
+        notifyDataSetChanged();
+    }
 }
--- app/src/main/java/by/madcat/development/f1newsreader/adapters/NewsListAdapter.java
+++ app/src/main/java/by/madcat/development/f1newsreader/adapters/NewsListAdapter.java
@@ -1,24 +1,15 @@
 package by.madcat.development.f1newsreader.adapters;
 
 import android.content.Context;
-import android.database.Cursor;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.bumptech.glide.Glide;
-
-import java.io.File;
-
 import by.madcat.development.f1newsreader.R;
-import by.madcat.development.f1newsreader.data.DatabaseDescription.News;
-import by.madcat.development.f1newsreader.dataInet.LoadNewsTask;
 
 public class NewsListAdapter extends NewsListAbstractAdapter{
 
-    private Cursor cursor;
     private final NewsListAbstractAdapter.ClickListener clickListener;
-    private Context context;
 
     public NewsListAdapter(ClickListener clickListener, Context context){
         this.clickListener = clickListener;
@@ -29,26 +20,5 @@ public class NewsListAdapter extends NewsListAbstractAdapter{
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.news_list_card, parent, false);
         return new ViewHolder(view, clickListener);
-    }
-
-    @Override
-    public void onBindViewHolder(final ViewHolder holder, int position) {
-        cursor.moveToPosition(position);
-        holder.setRowID(position);
-        holder.title.setText(cursor.getString(cursor.getColumnIndex(News.COLUMN_TITLE)));
-
-        String pathToImage = context.getFilesDir() +
-                "/" + LoadNewsTask.IMAGE_PATH + "/" + cursor.getString(cursor.getColumnIndex(News.COLUMN_IMAGE));
-        Glide.with(context).load(new File(pathToImage)).into(holder.thumbnail);
-    }
-
-    @Override
-    public int getItemCount() {
-        return (cursor != null) ? cursor.getCount() : 0;
-    }
-
-    public void swapCursor(Cursor cursor){
-        this.cursor = cursor;
-        notifyDataSetChanged();
     }
 }
