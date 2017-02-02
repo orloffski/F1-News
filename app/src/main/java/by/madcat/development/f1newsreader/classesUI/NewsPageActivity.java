e1c78038a38d55162e6e3bbe6c5946b8abe46304
Evgen Orlovsky <orloffski@gmail.com>
2017-02-02 08:57:15 +0300

    Rev 1.108.4 CollapsingToolbarLayout title on newsPage changed

--- app/src/main/java/by/madcat/development/f1newsreader/classesUI/NewsPageActivity.java
+++ app/src/main/java/by/madcat/development/f1newsreader/classesUI/NewsPageActivity.java
@@ -5,11 +5,12 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.widget.ImageView;
+
+import net.opacapp.multilinecollapsingtoolbar.CollapsingToolbarLayout;
 
 import java.util.ArrayList;
 
