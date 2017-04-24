571c498bb2127f59b003f9185999fb1548477ebc
MadCat <orloffski@gmail.com>
2017-04-24 17:43:00 +0300

    Rev 1.14.1 Update

--- app/src/main/java/by/madcat/development/f1newsreader/Services/BackgroundLoadNewsService.java
+++ app/src/main/java/by/madcat/development/f1newsreader/Services/BackgroundLoadNewsService.java
@@ -8,7 +8,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.BitmapFactory;
-import android.graphics.Color;
 import android.net.ConnectivityManager;
 import android.os.SystemClock;
 import android.support.v4.app.NotificationCompat;
