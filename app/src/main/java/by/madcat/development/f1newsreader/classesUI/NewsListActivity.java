f52356fd3e5b539b0dacde7b4a553c68a2c7b168
MadCat <orloffski@gmail.com>
2017-04-28 23:54:58 +0300

    Rev 1.15.7 SearchView bugs fixed

--- app/src/main/java/by/madcat/development/f1newsreader/classesUI/NewsListActivity.java
+++ app/src/main/java/by/madcat/development/f1newsreader/classesUI/NewsListActivity.java
@@ -97,6 +97,18 @@ public class NewsListActivity extends AppCompatActivity
             }
         });
 
+        searchView.setOnSearchViewListener(new MaterialSearchView.SearchViewListener(){
+            @Override
+            public void onSearchViewShown() {
+                openSectionNews(nowType, "");
+            }
+
+            @Override
+            public void onSearchViewClosed() {
+                openSectionNews(nowType, null);
+            }
+        });
+
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
