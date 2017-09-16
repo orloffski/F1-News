c11cccd9940e6bc8dd5693ad6c5bb96d42c474e7
MadCat <orloffski@gmail.com>
2017-09-16 21:24:16 +0300

    Rev 2.3.1.2 Remove unused methods

--- app/src/main/java/by/madcat/development/f1newsreader/classesUI/NewsListActivity.java
+++ app/src/main/java/by/madcat/development/f1newsreader/classesUI/NewsListActivity.java
@@ -14,7 +14,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -167,14 +166,6 @@ public class NewsListActivity extends AppCompatActivity implements NewsOpenListe
         }
 
         viewPager.setAdapter(adapter);
-    }
-
-    public View getTimerLink(){
-        return timer;
-    }
-
-    public View getTimerTextLink(){
-        return timerText;
     }
 
     public CoordinatorLayout getCoordinatorLayout(){
